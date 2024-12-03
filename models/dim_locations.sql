fa9c93b6b0babc81da0d7222bb312903669ab86e
Dolf ter Hofsté <dolf@terhofste.nl>
2024-12-03 20:45:52 +0100

    feat: add indian postal zones

--- models/dim_locations.sql
+++ models/dim_locations.sql
@@ -1,10 +1,17 @@
 select distinct
 
     {{ dbt_utils.generate_surrogate_key(['city', 'state', 
-    'postal_code', 'country']) }} as location_key,
-    city,
+    'postal_code', 'country']) }} as location_key,    city,
     state,
     postal_code,
-    country
+    country,
+    case
+        when postal_code[1] in ('1', '2') then 'north'
+        when postal_code[1] in ('3', '4') then 'west'
+        when postal_code[1] in ('5', '6') then 'south'
+        when postal_code[1] in ('7', '8') then 'east'
+        when postal_code[1] = '9' then 'APS'
+        else '<unknown>'
+    end as zone
 
 from {{ ref('stg_sales') }}
